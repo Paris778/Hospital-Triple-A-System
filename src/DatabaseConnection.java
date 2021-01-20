b4b080aec98bb4ebd7b28c773cfaa7f2f4666ba8
Paris778 <48055946+Paris778@users.noreply.github.com>
2021-01-20 13:41:34 +0000

    Hashing SHA-256, Password Strength evaluation, Strong Password Suggestion, Password Improvement Suggestions

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class DatabaseConnection {
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.*;
+
+public class DatabaseConnection {
+    private static final String JDBC_DRIVER = "org.sqlite.JDBC";
+    private static final String DATABASE_LOCATION = "jdbc:sqlite:";
+    private static String dbName = "patients.db";
+    private static Connection con = null;
+    private static PreparedStatement p = null;
+    private static ResultSet results = null;
+
+    private static void establishConnection() {
+        try {
+            // Load the JDBC driver
+            Class.forName(JDBC_DRIVER);
+
+            // Attempt to open a connection
+            con = DriverManager.getConnection(DATABASE_LOCATION + dbName);
+
+            // Execute SQL query
+            p = con.prepareStatement("SELECT * FROM patients");
+            results = p.executeQuery();
+
+            // Loop through each row and print
+            while (results.next()) {
+                int id = results.getInt("patient_id");
+                String forename = results.getString("forename");
+                String surname = results.getString("surname");
+                System.out.println(id + "\t\t" + forename + "\t\t" + surname);
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            establishConnection();
+            con.setAutoCommit(false);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import utility.*;
+
 public class Main {
 
     public static void main(String args[]) {
-        System.out.println("Hello Passwords");
+
+        //Make new instance of password handler. We can make it static if we want to.
+        PasswordHandler pass = new PasswordHandler();
+
+        //Checks the strength of the passed string . Returns a password score
+        int score = pass.checkPasswordStrength("Password");
+        System.out.println("Password score: " + score); //We could use it to print a strength meter (*****-------) or something
+
+        //If the password is not good, we print suggestions (more special characters etc)
+        pass.printPasswordImprovementSuggestions();
+
+        //We can also suggest strong passwords
+        System.out.println("Suggested strong password: " + pass.getStrongPassword());
     }
 }
