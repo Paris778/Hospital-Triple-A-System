4937e16ba28348cc977f15ad275126ceceedc58a
gruellan <ruellan.george@gmail.com>
2021-01-23 17:22:13 +0000

    Man I'm bad at merge conflicts

--- src/ServerImpl.java
+++ src/ServerImpl.java
@@ -1,7 +1,9 @@
 import java.rmi.RemoteException;
+import utility.*;
 
 public class ServerImpl extends java.rmi.server.UnicastRemoteObject implements ServerInterface {
     private static final long serialVersionUID = 1L;
+    private static final PasswordHandler passwordHandler = new PasswordHandler();
     private DatabaseConnection dbConnection;
 
     public ServerImpl() throws RemoteException {
@@ -16,5 +18,15 @@ public class ServerImpl extends java.rmi.server.UnicastRemoteObject implements S
 
     public void viewPatients() {
         dbConnection.viewPatients();
+    }
+
+    @Override
+    public boolean storeHashedPassword(byte[] hash) throws RemoteException {
+        return false;
+    }
+
+    @Override
+    public boolean varifyPassword(byte[] hash, int clientId) throws RemoteException {
+        return false;
     }
 }
--- src/ServerInterface.java
+++ src/ServerInterface.java
@@ -5,4 +5,8 @@ public interface ServerInterface extends java.rmi.Remote {
     void createUser(User user) throws RemoteException;
 
     void viewPatients() throws RemoteException;
+
+    abstract public boolean storeHashedPassword(byte[] hash) throws RemoteException;
+
+    abstract public boolean varifyPassword(byte[] hash, int clientId) throws RemoteException;
 }
