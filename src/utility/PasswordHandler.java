bde1be53a5233fbd05689be1159c37d8f1a491ce
Paris778 <48055946+Paris778@users.noreply.github.com>
2021-01-25 13:03:58 +0000

    Changed Hashing Algorithm from SHA-256 to SHA3-256

--- src/utility/PasswordHandler.java
+++ src/utility/PasswordHandler.java
@@ -15,63 +15,70 @@ import java.util.Random;
 //
 //////////////////////////////////////////////////////////////////////////////
 
-public class PasswordHandler {
+public class PasswordHandler{
+    
     //Variables
     StringBuilder suggestions = new StringBuilder();
 
     //Constructor
-    public PasswordHandler() {
+    public PasswordHandler(){
 
     }
 
     //
-    public String unhash(byte[] hash) {
+    public String unhash(byte[] hash){
         return "";
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    public int checkPasswordStrength(String hash) {
+    public int checkPasswordStrength(String hash){
         int passwordScore = 0;
-
+        
         //Check the length of password
-        if (hash.length() < Constants.MIN_PASSWORD_LENGTH) {
+        if(hash.length() < Constants.MIN_PASSWORD_LENGTH){
             this.suggestions.append("- Password should be more than 8 characters long.\n");
-        } else if (hash.length() >= Constants.ADEQUATE_PASSWORD_LENGTH) {
+        }
+        else if (hash.length() >= Constants.ADEQUATE_PASSWORD_LENGTH){
             passwordScore += 2;
-        } else {
+        }
+        else{
             passwordScore++;
         }
 
         //Check if password contains at least one number
-        if (hash.matches("(?=.*[0-9]).*")) {
+        if( hash.matches("(?=.*[0-9]).*")){
             passwordScore += 2;
-        } else {
+        }
+        else{
             this.suggestions.append("- Password should contain at least one number (123).\n");
         }
-
+        
         //Check if password contains lowercase letters
-        if (hash.matches("(?=.*[a-z]).*")) {
+        if( hash.matches("(?=.*[a-z]).*") ){
             passwordScore += 2;
-        } else {
+        }
+        else{
             this.suggestions.append("- Password should contain at least one lower case letter (abc).\n");
         }
-
-
+         
+        
         //Check if password contains upper case letters
-        if (hash.matches("(?=.*[A-Z]).*")) {
+        if( hash.matches("(?=.*[A-Z]).*") ){
             passwordScore += 2;
-        } else {
+        }
+        else{
             this.suggestions.append("- Password should contain at least one upper case letter (ABC).\n");
         }
-
-
+           
+        
         //Check if password contains special characters
-        if (hash.matches("(?=.*[~!@#$%^&*()_-]).*")) {
+        if(hash.matches("(?=.*[~!@#$%^&*()_-]).*") ){
             passwordScore += 2;
-        } else {
+        }
+        else{
             this.suggestions.append("- Password should contain at least one special character (!?<>,.).\n");
         }
-
+           
         hash = null; //Just a safety precaution so that the value doesn't stay in memory even though Java garbage collection should get it anyways 
         return passwordScore;
     }
@@ -79,23 +86,24 @@ public class PasswordHandler {
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //Prints suggestions to strengthen your password
-    public void printPasswordImprovementSuggestions() {
+    public void printPasswordImprovementSuggestions(){
         System.out.println(suggestions.toString());
         suggestions.setLength(0); //Clear string builder
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //Returns a strong password suggestion
-    public String getStrongPassword() {
+    public String getStrongPassword(){
 
         String testString = Constants.CAPITAL_LETTERS + Constants.LOWERCASE_LETTERS + Constants.NUMBERS + Constants.SPECIAL_CHARACTERS;
         Random rand = new Random();
-        char[] password = new char[Constants.ADEQUATE_PASSWORD_LENGTH];
+        char[] password = new char[Constants.ADEQUATE_PASSWORD_LENGTH]; 
 
-        for (int i = 0; i < Constants.ADEQUATE_PASSWORD_LENGTH; i++) {
+        for (int i = 0; i < Constants.ADEQUATE_PASSWORD_LENGTH; i++) 
+        { 
             password[i] = testString.charAt(rand.nextInt(testString.length()));
-        }
-
+        } 
+        
         System.out.println(password);
         return password.toString();
     }
@@ -109,7 +117,7 @@ public class PasswordHandler {
         byte[] salt = new byte[16];
         secRand.nextBytes(salt);
 
-        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        MessageDigest digest = MessageDigest.getInstance("SHA3-256");
         digest.update(salt); //Add salt to hash
         byte[] hashed_password = digest.digest(password.getBytes(StandardCharsets.UTF_8));
 
